0b8063e65fea3b838c49fb815b333f447f262462
LaDarrious <LaDarrious@LILDEE249>
2026-01-07 11:02:25 -0500

    Added lockallaccounts.png

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -172,6 +172,16 @@ namespace Risk_Manager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap lockallaccounts {
+            get {
+                object obj = ResourceManager.GetObject("lockallaccounts", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap locksettings {
             get {
                 object obj = ResourceManager.GetObject("locksettings", resourceCulture);
