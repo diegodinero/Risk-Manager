b3cc29a208b7723b6c23d69a990f384077cec388
LaDarrious <LaDarrious@LILDEE249>
2026-01-09 00:47:04 -0500

    added one final leave image

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -210,6 +210,16 @@ namespace Risk_Manager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap leavesix {
+            get {
+                object obj = ResourceManager.GetObject("leavesix", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap leavetwo {
             get {
                 object obj = ResourceManager.GetObject("leavetwo", resourceCulture);
